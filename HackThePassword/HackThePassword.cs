0f62c29f71d48d8900bd0730646101d7dbcacf74
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-04-10 22:22:41 +0200

    optimalized

--- HackThePassword/HackThePassword.cs
+++ HackThePassword/HackThePassword.cs
@@ -11,61 +11,50 @@ namespace HackThePassword
             byte numberOfTestCases = Convert.ToByte(Console.ReadLine());
             for (byte i = 0; i < numberOfTestCases; i++)
             {
-                string inputString = Console.ReadLine();
+                string input = Console.ReadLine();
                 List<char> leftResults = new List<char>();
                 List<char> rightResults = new List<char>();
                 var finalResult = new StringBuilder();
 
-                for (int j = 0; j < inputString.Length; j++)
+                for (int j = 0; j < input.Length; j++)
                 {
-                    if (inputString[j] == '<')
+                    if (input[j] == '<')
                     {
                         if (leftResults.Count > 0)
                         {
                             char tmp = leftResults[leftResults.Count - 1];
                             rightResults.Add(tmp);
                             leftResults.RemoveAt(leftResults.Count - 1);
-                            continue;
                         }
-                        continue;
                     }
 
-                    else if (inputString[j] == '>')
+                    else if (input[j] == '>')
                     {
                         if (rightResults.Count > 0)
                         {
                             char tmp = rightResults[rightResults.Count - 1];
                             leftResults.Add(tmp);
                             rightResults.RemoveAt(rightResults.Count - 1);
-                            continue;
                         }
-                        continue;
                     }
 
-                    else if (inputString[j] == '-')
+                    else if (input[j] == '-')
                     {
                         if (leftResults.Count > 0)
                         {
                             leftResults.RemoveAt(leftResults.Count - 1);
-                            continue;
                         }
-                        continue;
                     }
 
                     else
                     {
-                        leftResults.Add(inputString[j]);
-                        continue;
+                        leftResults.Add(input[j]);
                     }
                 }
 
                 rightResults.Reverse();
                 leftResults.AddRange(rightResults);
-
-                foreach (char result in leftResults)
-                {
-                    finalResult.Append(result.ToString());
-                }
+                finalResult.Append(leftResults.ToArray());
 
                 Console.WriteLine(finalResult);
             }
